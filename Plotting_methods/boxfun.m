81f5872562906e6af5c5ed6c8068f007606e48dc
Emmanouil Froudarakis <eman@emac.lan>
2018-09-23 08:22:35 -0500

    boxfun

--- Plotting_methods/boxfun.m
+++ Plotting_methods/boxfun.m
@@ -1,4 +1,4 @@
-function hout = barfun(data,varargin)
+function hout = boxfun(data,varargin)
 
 % function barfun(data,varargin)
 %
@@ -14,63 +14,79 @@ params.markersize = 0.5;
 params.names = [];
 params.angle = 45;
 params.sig = 1;
-params.bar = 1;
 params.error = 'sde';
 params.colors = [];
-params.barwidth = 1;
+params.barwidth = 0.5;
 params.test = 'anovan';
-params.range = 0.5;
+params.barrange = 0.5;
 params.edgeColors = [];
+params.alpha = 0.7;
+params.range = [25 75;10 90;5 95;1 99;0 100];
+params.datacolor = [0.8 0.8 0.8];
+params.linecolor = [1 0 0];
+params.linewidth = 2;
+params.figure = [];
 
 params = getParams(params,varargin);
 
-% data = data';
-values = cellfun(@nanmean,data);
-if strcmp(params.error,'sde')
-    errors = cellfun(@(x) nanstd(x)/sqrt(length(x)),data);
-elseif  strcmp(params.error,'std')
-    errors = cellfun(@(x) nanstd(x),data);
+%%
+if isempty(params.figure)
+    figure;
+else
+    figure(params.figure)
 end
+
+% data = data';
+values = cellfun(@nanmedian,data);
 
 [nRows, nCols] = size(values);
 
 if nCols == 1 || nRows ==1
     loc = 1:length(values);
 else
-    loc = bsxfun(@plus,repmat(linspace(1-params.range/2,1+params.range/2,nCols),nRows,1),(1:nRows)'-1);
+    loc = bsxfun(@plus,repmat(linspace(1-params.barrange/2,1+params.barrange/2,nCols),nRows,1),(1:nRows)'-1);
 end
 
-%%%%%% edit for matlab 2014b
 if isempty(params.colors)
     params.colors = cbrewer('qual','Pastel1',max([nCols,3]));
 end
+
 if isempty(params.edgeColors)
     params.edgeColors = repmat('none',nCols,1);
 end
 
+%%
+gradient = [1 1.6 3 6 20];
 for i = 1:nCols
-    handles.bar(i) = bar(loc(:,i),values(:,i),'barwidth',params.barwidth/nCols,...
-        'faceColor',params.colors(i,:),'edgeColor',params.edgeColors(i,:)); % standard implementation of bar fn
+    for k = 1:nRows
     hold on
-end
-
-if nRows > 1
-    %     loc = nan(nRows,nCols);
-    for col = 1:nCols
-        % Extract the x location data needed for the errorbar plots:
-        %         loc(:,col) = mean(get(get(handles.bar(col),'children'),'xdata'),1);
-        % Use the mean x values to call the standard errorbar fn; the
-        % errorbars will now be centred on each bar:
-        errorbar(loc(:,col),values(:,col),errors(:,col), '.','color',[0.3 0.3 0.3])
+    plot(normrnd(0,0.1,length(data{k,i}),1)*params.barwidth + loc(k,i),data{k,i},'.','color',params.datacolor);
+    max_old = values(k,i);
+    min_old = values(k,i);
+    for iarea = 1:size(params.range,1)
+        mins = prctile(data{k,i},params.range(iarea,1));
+        maxs = prctile(data{k,i},params.range(iarea,2));
+        X = [-1 1 1 -1]*params.barwidth/2/gradient(iarea) + loc(k,i);
+        Y = [max_old max_old maxs maxs];
+        patch(X,Y,params.colors(i,:),'edgeColor',params.edgeColors(i,:),'facealpha',params.alpha);
+        Y = [mins mins min_old min_old];
+        patch(X,Y,params.colors(i,:),'edgeColor',params.edgeColors(i,:),'facealpha',params.alpha);
+        max_old = maxs;
+        min_old = mins;
     end
-else
-    %     loc = mean(get(get(handles.bar,'children'),'xdata'),1);
-    errorbar(loc,values,errors,'.','color',[0.3 0.3 0.3])
+    %     handles.bar(i) = bar(loc(:,i),values(:,i),'barwidth',params.barwidth/nRows,...
+    %         'faceColor',params.colors(i,:),'edgeColor',params.edgeColors(i,:)); % standard implementation of bar fn
+    plot([-params.barwidth/2 params.barwidth/2] + loc(k,i),[values(i) values(i)],'color',params.linecolor,'linewidth',params.linewidth)
+    end
 end
 
-mx = max((values(:)) + errors(:));
-if params.bar;mx = max([0 mx]);end
-vsp = ( max(abs(values(:)) + errors(:)))*0.1;
+
+%%
+
+mx = max(reshape(cellfun(@(x) prctile(x,99),data),[],1));
+mn = min(reshape(cellfun(@(x) prctile(x,1),data),[],1));
+vsp = [mx-mn]*0.05;
+
 if params.sig
     df =  mean(mean(diff(loc')));
     hsp = df*0.1;
@@ -78,6 +94,7 @@ if params.sig
         data = data';
         [nRows, nCols] = size(data);
     end
+    
     for iRow = 1:nRows
         [~,seq] = sort(pdist(reshape(loc(iRow,:),[],1)));
         
@@ -94,22 +111,7 @@ if params.sig
         xd = loc(yind(seq)) - loc(xind(seq));
         uni = unique(xd);
         space = xd;
-%         for i = 2:length(uni)
-%             pairs = nchoosek(find(xd==uni(i)),2);
-%             if size(pairs,2)>1
-%                 for ipair = 1:size(pairs,1)
-%                     [~,xi] = sort(x1(pairs(ipair,:)));
-%                     if x1(pairs(ipair,xi(2)))<x2(pairs(ipair,xi(1))) && ...
-%                             space(pairs(ipair,xi(2))) == space(pairs(ipair,xi(1)))
-%                         indx = false(size(space));
-%                         indx(pairs(ipair,xi(2))) = true;
-%                         indx(xd>uni(i)) = true;
-%                         space(indx) = space(indx)+1;
-%                     end
-%                 end
-%             end
-%         end
-%         
+        
         % plot the erros if significant
         if strcmp(params.test,'anovan')
             C = [];
@@ -124,8 +126,8 @@ if params.sig
                 [sig, p] = eval([params.test '(data{iRow,xind(seq(iPair))},' ...
                     'data{iRow,yind(seq(iPair))},params.thr)']);
             else
-               p = stat(stat(:,1)==xind(seq(iPair)) & stat(:,2)==yind(seq(iPair)),6);
-               sig = p<params.thr;
+                p = stat(stat(:,1)==xind(seq(iPair)) & stat(:,2)==yind(seq(iPair)),6);
+                sig = p<params.thr;
             end
             
             if ~isnan(sig) && sig
@@ -141,7 +143,9 @@ if params.sig
         end
     end
     
-    set(gca,'ylim',[min([0 min(values(:) - 2*errors(:))]) mx+vsp*(nCols+1)])
+%     set(gca,'ylim',[min([0 min(values(:))]) mx+vsp*(nCols+1)])
+%     ylim([mn - vsp mx+vsp*max(space(iPair))])
+
 end
 
 set(gca,'Box','Off');
@@ -153,22 +157,11 @@ else
 end
 if ~isempty(params.names)
     set(gca,'XTickLabel',params.names)
-%     if nCols==1
-        ht = xticklabel_rotate([],params.angle,[]);
-        set(ht,'HorizontalAlignment','right')
-%     end
+    ht = xticklabel_rotate([],params.angle,[]);
+    set(ht,'HorizontalAlignment','right')
 end
 
-if ~params.bar
-    delete(handles.bar)
-    handles.bar = plot(values);
-    set(gca,'xtick',1:length(values))
-end
 hold off
-
-if nargout
-    hout = handles.bar;
-end
 
 function ast = pval(p)
 if p<0.001
